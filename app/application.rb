a0af0ae54c8934f52609192aea068cc42bb6ff74
Zach  Smith <zdsmith001@gmail.com>
2019-10-29 21:31:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,14 @@ def call(env)
   req = Rack::Request.new
 
   if req.path == "/items/<ITEM NAME>"
+    Item.all.detect do |item|
+      if item.name == <ITEM NAME>
+        item.price
+      else
+        resp.write "We don't have that item"
+        resp.status = 404
+      end
+    end
   else
     resp.write "Route not found"
     resp.status = 404
