d167d75547fa93e9849165e8f7e7f2ad05bb5a17
Zach  Smith <zdsmith001@gmail.com>
2019-10-29 21:37:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,14 @@
 class Application
 
+@@items = []
+
 def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
 
   if req.path.match(/items/)
     item_name = req.path.split("/items/").last
-    Item.all.detect do |item|
+    @@items.detect do |item|
       if item.name == item_name
         item.price
       else
