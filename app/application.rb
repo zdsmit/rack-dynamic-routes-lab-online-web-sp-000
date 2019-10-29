50fc9883e83ac0b7daad16946c362e6c69eb2267
Zach  Smith <zdsmith001@gmail.com>
2019-10-29 21:33:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
 
 def call(env)
   resp = Rack::Response.new
-  req = Rack::Request.new
+  req = Rack::Request.new(env)
 
   if req.path == "/items/"
     item_name = req.path.split("/items/").last
